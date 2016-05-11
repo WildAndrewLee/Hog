a1b8713ae2ff1da3ffa7efb11e3ffb0aeb9b6118
Andrew <andrew.dragon.lee@gmail.com>
2016-05-11 02:17:46 -0400

    remove test info from main

--- src/main.go
+++ src/main.go
@@ -2,24 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"hog"
-	"logger"
-	"network/opcodes"
 )
 
 func main() {
 	flag.Parse()
-
-	test := []string{"Andrew"}
-
-	testBuffer := hog.NewMessage(opcodes.SendMessage, test...)
-
-	logger.Info.Println(logger.FormatByteSlice(testBuffer))
-
-	msg := hog.ParseMessage(testBuffer[2:])
-
-	fmt.Printf("%+v\n", msg)
-
 	hog.Start()
 }
